a201c309e46db83defc786db291752e65b42799e
JZelinsky3 <89413031+JZelinsky3@users.noreply.github.com>
2023-09-18 00:07:55 -0400

    fix

--- assignments/assignment 6/script.js
+++ assignments/assignment 6/script.js
@@ -41,3 +41,20 @@ window.onload = () => {
   document.getElementById("hide").onclick = hideImg;
   document.getElementById("show").onclick = showImg;
 }
+
+var id = null;
+function myMove() {
+  var elem = document.getElementById("myAnimation");   
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + 'px'; 
+      elem.style.left = pos + 'px'; 
+    }
+  }
+}
