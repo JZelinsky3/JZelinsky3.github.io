d329f2a585cdd1cdd346e4af92a4fb32b92950ab
JZelinsky3 <89413031+JZelinsky3@users.noreply.github.com>
2023-09-17 23:51:08 -0400

    assignment 6

---
+++ assignments/assignment 6/script.js
@@ -0,0 +1,43 @@
+var moving = 0
+const animate = () => {
+
+  if (moving > 0) {
+    document.getElementById("icon").classList.remove("animate");
+    moving = 0
+  } else {
+    document.getElementById("icon").classList.add("animate");
+    moving = 1
+   }
+}
+
+
+function elementFromHtml(html) {
+  console.log(html);
+  const template = document.createElement("template");
+  template.innerHTML = html.trim();
+  return template.content.firstElementChild;
+}
+
+const displayText = () => {
+  const productName = document.getElementById("product-name").value;
+  const comment = document.getElementById("comment").value;
+  const rating = document.getElementById("rating").value;
+  const username = document.getElementById("username").value;
+  html = '<section class="toutput"> <h3>'+ productName +'</h3> <p>'+ rating + '/5 stars ' + comment + '</p><p>' + username +'by </p></section>'
+  html = elementFromHtml(html);
+  document.getElementById("responses").appendChild(html);
+}
+
+const showImg = () => {
+  document.getElementById("img").classList.remove("hide");
+}
+const hideImg = () => {
+  document.getElementById("img").classList.add("hide");
+}
+
+window.onload = () => {
+  document.getElementById("animate-button").onclick = animate;
+  document.getElementById("add-comment").onclick = displayText;
+  document.getElementById("hide").onclick = hideImg;
+  document.getElementById("show").onclick = showImg;
+}
