dae4e656440cbb8c0d81a98eb67b29a24fa985b1
JZelinsky3 <89413031+JZelinsky3@users.noreply.github.com>
2023-10-10 01:34:04 -0400

    fixed

--- projects/part1/script.js
+++ projects/part1/script.js
@@ -1,8 +1,13 @@
-// JavaScript to handle hamburger menu toggle
-
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const mainNav = document.getElementById('mainNav');
 
 hamburgerMenu.addEventListener('click', () => {
     mainNav.classList.toggle('show');
 });
+
+const displayText = () => {
+    const productName = document.getElementById("product-name").value;
+    html = '<section class="toutput"> <h3>'+ productName +'</h3>'
+    html = elementFromHtml(html);
+    document.getElementById("responses").appendChild(html);
+  }
