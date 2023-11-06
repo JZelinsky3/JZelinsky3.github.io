ca1c44a7bd7862687f2680b9f451b71b7b8eebdb
JZelinsky3 <89413031+JZelinsky3@users.noreply.github.com>
2023-11-05 21:52:46 -0500

    assignment 13 node

---
+++ node/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+
+app.get('/',(req, res) => {
+    res.sendFile(__dirname + "/index.html");
+}); 
+
+    app.get("/api/data", (req, res) => {
+        
+        const data = [
+            {
+            id: 1,
+            title: 'Back in Black',
+            artist: 'AC/DC',
+            year: 1980,
+            tracks: ['Hells Bells', 'Shoot to Thrill', 'Back in Black'],
+            image: 'img/bib.png',
+          },
+          {
+            id: 2,
+            title: 'Led Zeppelin IV',
+            artist: 'Led Zeppelin',
+            year: 1971,
+            tracks: ['Stairway to Heaven', 'Black Dog', 'Rock and Roll'],
+            image: 'img/lz4.jpg',
+          },
+          {
+            id: 3,
+            title: 'Nevermind',
+            artist: 'Nirvana',
+            year: 1991,
+            tracks: ['Smells Like Teen Spirit', 'In Bloom', 'Come as You Are'],
+            image: 'img/never.jpg',
+          },
+          {
+            id: 4,
+            title: 'The Dark Side of the Moon',
+            artist: 'Pink Floyd',
+            year: 1973,
+            tracks: ['Time', 'Money', 'Us and Them'],
+            image: 'img/dsotm.jpg',
+          }
+        ];
+    
+        res.json(data);
+    });
+    
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
