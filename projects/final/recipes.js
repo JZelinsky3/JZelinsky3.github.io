1d5112a69199cf420d42e9fbbf4ff8d7d63f5e2d
JZelinsky3 <89413031+JZelinsky3@users.noreply.github.com>
2023-12-10 05:32:44 -0500

    bug fix

--- projects/final/recipes.js
+++ projects/final/recipes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
-const Recipe = require('./models/recipeModel');
+const fs = require('fs/promises');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,11 +11,32 @@ mongoose.connect("mongodb+srv://jzelinsky18:nSzIkd4O4vtyOCo0@cluster0.0fwvmjy.mo
     .then(() => console.log("Connected to MongoDB..."))
     .catch(err => console.error("Could not connect to MongoDB...", err));
 
+const Recipe = mongoose.model('Recipe', {
+    title: String,
+    image: String,
+    description: String,
+    author: String,
+    difficulty: String,
+    cookTime: String,
+    ingredients: [String],
+    instructions: [String]
+});
+
 app.use(bodyParser.json());
 
 app.get('/recipes', async (req, res) => {
+    const { meal, category, title } = req.query;
     try {
-        const recipes = await Recipe.find();
+        let recipes;
+
+        if (title) {
+            recipes = await Recipe.find({ title });
+        } else if (meal) {
+            recipes = await Recipe.find({ meal, category });
+        } else {
+            recipes = await Recipe.find();
+        }
+
         res.json(recipes);
     } catch (err) {
         console.error(err);
@@ -23,59 +44,16 @@ app.get('/recipes', async (req, res) => {
     }
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'path', 'to', 'your', 'recipes.html'));
-});
 
-document.addEventListener('DOMContentLoaded', () => {
-    const breakfastButton = document.querySelector('.dropbtn');
-    const dropContent = document.querySelector('.drop-content');
-
-    breakfastButton.addEventListener('click', (event) => {
-        dropContent.classList.toggle('show');
-    });
-
-    dropContent.addEventListener('click', (event) => {
-        if (event.target.tagName === 'A') {
-            const category = event.target.textContent.toLowerCase();
-            fetchRecipesByCategory(category);
-        }
-    });
-});
-
-const fetchRecipesByCategory = async (category) => {
+async function readRecipesFromJsonFile(filePath) {
     try {
-        const response = await fetch(`/recipes?category=${category}`);
-        const recipes = await response.json();
-        displayRecipes(recipes);
+        const content = await fs.readFile(filePath, 'utf-8');
+        const recipes = JSON.parse(content);
+        return recipes;
     } catch (error) {
-        console.error('Error fetching recipes:', error);
+        console.error('Error reading recipes from JSON file:', error);
+        return [];
     }
-};
-
-const displayRecipes = (recipes) => {
-    const recipeContainer = document.getElementById('recipeContainer');
-    recipeContainer.innerHTML = '';
-
-    recipes.forEach(recipe => {
-        const recipeElement = document.createElement('div');
-        recipeElement.innerHTML = `
-            <h2>${recipe.title}</h2>
-            <img src="${recipe.image}" alt="${recipe.title}" style="width: 400px; height: 200px;">
-            <p><strong>Description:</strong> ${recipe.description}</p>
-            <p><strong>Author:</strong> ${recipe.author}</p>
-            <p><strong>Difficulty:</strong> ${recipe.difficulty}</p>
-            <p><strong>Cook Time:</strong> ${recipe.cookTime}</p>
-        
-            <h3>Ingredients</h3>
-            <ul>${recipe.ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}</ul>
-        
-            <h3>Instructions</h3>
-            <ol>${recipe.instructions.map(instruction => `<li>${instruction}</li>`).join('')}</ol>
-            <hr>`;
-
-        recipeContainer.appendChild(recipeElement);
-    });
-};
+}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
